fe9a936ed644401a8445b6f0464f20800a939274
Asia Sageman <asageman@macalester.edu>
2017-08-14 22:56:37 -0500

    pushing school map

---
+++ js/dataSchools.js
@@ -0,0 +1,498 @@
+//public PRESCHOOL points
+publicPreschool = [
+{
+  "name": "Joseph E Hill Education Center",
+  "lat": 42.045047,
+  "lng": -87.708670,
+  "website": "https://www.district65.net/domain/156",
+  "url": "http://www.tuw.org/sites/tuw.org/files/Preschool%20Photo.jpg",
+  "text": "The Joseph E Hill Education Center is a public preschool.",
+  "type": "publicpreschool"
+}
+];
+
+
+//public ELEMENTARY SCHOOL points
+publicElementary = [
+{
+  "name": "Dewey Elementary School",
+  "lat": 42.045640,
+  "lng": -87.691177,
+  "website": "https://www.district65.net/dewey",
+  "url": "https://thoughtcatalog.files.wordpress.com/2015/03/shutterstock_83691820.jpg?w=786&h=524",
+  "text": "Dewey is a public elementary school.",
+  "type": "publicelementary"
+},
+{
+  "name": "Lincolnwood Elementary School",  
+  "lat": 42.060392,
+  "lng": -87.709502,
+  "website": "https://www.district65.net/Domain/15",
+  "url": "https://healthbeat.spectrumhealth.org/wp-content/uploads/2015/04/Study-time.jpg",
+  "text": "Lincolnwood is a public elementary school.",
+  "type": "publicelementary"
+},
+{
+  "name": "Orrington Elementary School",
+  "lat": 42.065492,
+  "lng": -87.679900,
+  "website": "https://www.district65.net/orrington",
+  "url": "http://cdn.skim.gs/images/v1/msi/r63jbooxlqsov4y0dhzy/Teaching-kids-how-to-make-friends-at-school",
+  "text": "Orrington is a public elementary school.",
+  "type": "publicelementary"
+},
+{
+  "name": "Willard Elementary School",  
+  "lat": 42.066852,
+  "lng": -87.717019,
+  "website": "https://www.district65.net/willard",
+  "url": "http://www.ahaparenting.com/img/iStock_000012290461XSmall.jpg",
+  "text": "Willard is a public elementary school.",
+  "type": "publicelementary"
+},
+{
+  "name": "Kingsley Elementary School",  
+  "lat": 42.060024,
+  "lng": -87.695966,
+  "website": "https://www.district65.net/kingsley",
+  "url": "http://www.ashfordparkschool.com/communities/8/004/011/971/738//images/4594671976.jpg",
+  "text": "Kingsley is a public elementary school.",
+  "type": "publicelementary"
+},
+{
+  "name": "Lincoln Elementary School",  
+  "lat": 42.034367,
+  "lng": -87.675630,
+  "website": "https://www.district65.net/lincoln",
+  "url": "https://www.livehappy.com/sites/default/files/styles/article_featured/public/main/articles/Happy-Schools-main-2.jpg?itok=oK609_VO",
+  "text": "Lincoln is a public elementary school.",
+  "type": "publicelementary"
+},
+{
+  "name": "Washington Elementary School",  
+  "lat": 42.034816,
+  "lng": -87.693716,
+  "website": "https://www.district65.net/washington",
+  "url": "https://www.parentmap.com/images/article/7575/elementary_kids.jpg",
+  "text": "Washington is a public elementary school.",
+  "type": "publicelementary"
+},
+{
+  "name": "Dawes Elementary Schools",  
+  "lat": 42.025973,
+  "lng": -87.700137,
+  "website": "https://www.district65.net/dawes",
+  "url": "http://cdn.skim.gs/image/upload/v1456338776/msi/school-bus_vnfvne.jpg",
+  "text": "Dawes is a public elementary school.",
+  "type": "publicelementary"
+},
+{
+  "name": "Oakton Elementary School",  
+  "lat": 42.026203,
+  "lng": -87.686141,
+  "website": "https://www.district65.net/oakton",
+  "url": "http://p5cdn4static.sharpschool.com/UserFiles/Servers/Server_3032574/Image/Website/Kindergarten.jpg",
+  "text": "Oakton is a public elementary school.",
+  "type": "publicelementary"
+}
+];
+
+//public MIDDLE SCHOOL points
+publicMiddle = [
+{
+  "name": "Haven Middle School",
+  "lat": 42.061575,
+  "lng": -87.697928,
+  "website": "https://www.district65.net/Domain/9",
+  "url": "http://www.shawanoschools.com/communityed/pictures/kids%20middle%20school.jpg",
+  "text": "Haven is a public middle school.",
+  "type": "publicmiddle"
+},
+{
+  "name": "Nichols Middle School",
+  "lat": 42.037255,
+  "lng": -87.683069,
+  "website": "https://www.district65.net/Domain/10",
+  "url": "https://greatergood.berkeley.edu/images/application_uploads/Middle_school_discussion.jpg",
+  "text": "Nichols is a public middle school.",
+  "type": "publicmiddle"
+},
+{
+  "name": "Chute Middle School",
+  "lat": 42.026244,
+  "lng": -87.692675,
+  "website": "https://www.district65.net/chute",
+  "url": "http://www.ophtek.com/wp-content/uploads/2014/08/Computer-Lab-Happy-Kids.jpg",
+  "text": "Chute is a public middle school.",
+  "type": "publicmiddle"
+}
+];
+
+//public K-8 SCHOOL points
+publicKEight = [
+{
+  "name": "Martin Luther King Jr. Literary and Fine Arts School",
+  "lat": 42.043676,
+  "lng": -87.707953,
+  "website": "https://www.district65.net/Domain/21",
+  "url": "http://www.buildingstrongerchildren.com/uploads/2/8/3/7/28372547/5573003_orig.jpg",
+  "text": "King Arts is a public magnet school for kindergarten-8th grade students.",
+  "type": "publick-8"
+},
+{
+  "name": "Dr. Bessie Rhodes School of Global Studies",
+  "lat": 42.047407,
+  "lng": -87.718507,
+  "website": "https://www.district65.net/Domain/22",
+  "url": "https://mkr44k6om2-flywheel.netdna-ssl.com/wp-content/uploads/2016/08/back-to-school.jpg",
+  "text": "Bessie Rhodes is a public magnet school for kindergarten-8th grade students.",
+  "type": "publick-8"
+}
+];
+
+//public HIGH SCHOOL points
+publicHigh = [
+{
+  "name": "Evanston Township High School",
+  "lat": 42.046250,
+  "lng": -87.701121,
+  "website": "https://www.eths.k12.il.us/",
+  "url": "http://www.trbimg.com/img-57446d36/turbine/ct-evr-evanston-high-school-graduation-tl-0526-20160523",
+  "text": "Evanston Township High School is Evanston's one and only public high school.",
+  "type": "publichigh"
+}
+];
+
+//public SPECIAL SERVICE SCHOOL points
+publicSpecialService = [
+{
+  "name": "Rice Children's Center (grades 3-8)",
+  "lat": 42.033315,
+  "lng": -87.687135,
+  "website": "https://www.district65.net/domain/436",
+  "url": "https://www.childrenshomeandaid.org/wp-content/uploads/2016/12/RiceVideoThumbnail.png",
+  "text": "Rice is a public theraputic school for 3rd-8th grade students.",
+  "type": "publicspecialservice"
+},
+{
+  "name": "Park School (grades PK-12)",
+  "lat": 42.033805,
+  "lng": -87.683330,
+  "website": "https://www.district65.net/domain/212",
+  "url": "https://www.district65.net/cms/lib/IL01906289/Centricity/ModuleInstance/433/park-banner1.jpg",
+  "text": "Park is a public school for preschool-12th graders that provides special needs education.",
+  "type": "publicspecialservice"
+}
+];
+
+//private PRESCHOOL points
+privatePreschool = [
+{
+  "name": "Midwest Montessori",
+  "lat": 42.058292,
+  "lng": -87.684416,
+  "website": "http://midwestmontessori.com/",
+  "url": "http://midwestmontessori.com/wp-content/uploads/2013/07/Children1-980x350.png",
+  "text": "Midwest Montessori is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "The Barbereux School",
+  "lat": 42.063840,
+  "lng": -87.727014,
+  "website": "http://www.barbereux.com/",
+  "url": "http://www.daycarechicago.org/wp-content/uploads/2015/07/Evanston_The_Barbereux_School.jpg",
+  "text": "The Barbereux School is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Unity Preschool",
+  "lat": 42.054041,
+  "lng": -87.683213,
+  "website": "http://www.unitypreschoolevanston.com/",
+  "url": "https://d1z3744m6z2h75.cloudfront.net/aws/fs/565/1369565_r3XGh4ROlYohQvirD4gLtMd_ECCeEiM9536YOiJZjCASjUa23MtNlnRDfGRbWlvTLK-hrGtUDzedlUZMGsdfdg...jpg",
+  "text": "Unity is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Northshore Child Development Center",
+  "lat": 42.063982,
+  "lng": -87.685171,
+  "website": "https://child-care-preschool.brighthorizons.com/il/evanston/northshore",
+  "url": "https://www.jle-ns.org/wp-content/uploads/slide2-972x406.jpg",
+  "text": "Northshore Child Development Center is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Mcgaw YMCA Child Care Center",
+  "lat": 42.043642,
+  "lng": -87.685469,
+  "website": "http://www.mcgawymca.org/youth-teens/childrens-center/",
+  "url": "http://www.trbimg.com/img-5949bb99/turbine/ct-evr-mcgaw-ymca-head-start-program-tl-0622-20170620",
+  "text": "Mcgaw YMCA Child Care Center is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Beth Emet Early Childhood Program",
+  "lat": 42.040980,
+  "lng": -87.688776,
+  "website": "http://bethemet.org/learning/early-childhood-programs.html",
+  "url": "http://w1.chabad.org/images/shluchim/minisites/themes/preschool/preschool_banner_image.jpg",
+  "text": "Beth Emet Early Childhood Program is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Warren W Cherry Preschool",
+  "lat": 42.044064,
+  "lng": -87.693069,
+  "website": "https://cherrypreschool.org/",
+  "url": "http://chambermaster.blob.core.windows.net/images/customers/1114/members/959/photos/GALLERY_MAIN/park_kids_1.jpeg",
+  "text": "Cherry is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Parent's Day Out",
+  "lat": 42.043837,
+  "lng": -87.685378,
+  "website": "http://www.pdoevanston.org/",
+  "url": "https://assets.babycenter.com/ims/2016/11/iStock_86482075_4x3.jpg?width=700",
+  "text": "Parents Day Out is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Children’s Montessori House",
+  "lat": 42.043470,
+  "lng": -87.679054,
+  "website": "http://www.evanstonmontessori.com/",
+  "url": "https://www.chiaravalle.org/uploaded/photos/Slideshow-yr2-7-16/Home-2016-7-C-cropped.jpg?5013",
+  "text": "Children’s Montessori House is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "School for Little Children",
+  "lat": 42.043598,
+  "lng": -87.680162,
+  "website": "http://slcevanston.org/",
+  "url": "http://slcevanston.org/2016newsite/wp-content/uploads/2016/06/slc-parent-involvement.jpg",
+  "text": "School for Little Children is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Toddler Town Day Care",
+  "lat": 42.019680,
+  "lng": -87.693570,
+  "website": "https://toddlertownchicago.com/",
+  "url": "https://toddlertownchicago.com/wp-content/uploads/2013/06/Toddler-Town-Evanston-3-year-olds-0101.jpg",
+  "text": "Toddler Town Day Care is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Child Care Center of Evanston",
+  "lat": 42.051821,
+  "lng": -87.690602,
+  "website": "http://www.childcarecenterofevanston.org/",
+  "url": "http://www.chicagoparent.com/downloads/7078/download/120452-img_0365.jpe?cb=aec056e161a725a33b5897ea149ba29d&w=620",
+  "text": "Child Care Center of Evanstonis a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "KinderCare Learning Center",
+  "lat": 42.040116,
+  "lng": -87.702894,
+  "website": "http://www.kindercare.com/our-centers/evanston/il/070462",
+  "url": "http://bluegumhill.com/wp-content/uploads/2017/02/526815574.jpg",
+  "text": "KinderCare Learning Center is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Covenant Nursery School",
+  "lat": 42.054844,
+  "lng": -87.719783,
+  "website": "http://www.covenantnurseryschool.org/",
+  "url": "http://cesrockville.org/uploads/preschool%201.jpg",
+  "text": "Covenant Nursery School is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Baby Toddler Nursery",
+  "lat": 42.033500,
+  "lng": -87.704542,
+  "website": "http://www.iwse.org/programs/baby-toddler-nursery/",
+  "url": "http://www.iwse.org/wp-content/uploads/2011/08/home_banner1_care.jpg",
+  "text": "Baby Toddler Nursery is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Northminster Nursery School",
+  "lat": 42.062921,
+  "lng": -87.717353,
+  "website": "https://nnsevanston.com/",
+  "url": "https://nnsevanston.files.wordpress.com/2016/01/summer-camp-the-bus-is-leaving.jpg",
+  "text": "Northminster Nursery School is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Jewish Reconstructionist Child Care",
+  "lat": 42.023233,
+  "lng": -87.699127,
+  "website": "https://www.jrc-evanston.org/education/early-childhood",
+  "url": "https://www.jrc-evanston.org/sites/default/files/opening_day_rollerimage.jpg",
+  "text": "Jewish Reconstructionist Child Care is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Total Child Center",
+  "lat": 42.047017,
+  "lng": -87.677953,
+  "website": "http://www.totalchild.org/",
+  "url": "http://www.totalchild.org/images/enroll.jpg",
+  "text": "Total Child Center is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Reba Early Learning Center",
+  "lat": 42.031875,
+  "lng": -87.680493,
+  "website": "http://www.rebaearlylearningcenter.com/",
+  "url": "https://www.todaysparent.com/wp-content/uploads/2011/10/getting-the-words-right-April08-iStock.jpg",
+  "text": "Reba Early Learning Center is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Puerta Abierta Preschool",
+  "lat": 42.034880,
+  "lng": -87.679235,
+  "website": "http://puertaabiertapreschool.org/",
+  "url": "http://natik.org/wp-content/uploads/2012/08/IMG_1974.jpg",
+  "text": "Puerta Abierta is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Bright Horizons",
+  "lat": 42.047529,
+  "lng": -87.680614,
+  "website": "https://child-care-preschool.brighthorizons.com/il/evanston/evanston",
+  "url": "http://www.orringtonplaza.com/gallery/large/BrightHorizons.jpg",
+  "text": "Bright Horizons is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Child Time",
+  "lat": 42.024392,
+  "lng": -87.684224,
+  "website": "https://www.childtime.com/your-local-school/evanston-il-9107/",
+  "url": "http://slcevanston.org/2016newsite/wp-content/uploads/2016/06/2s-beans.jpg",
+  "text": "Child Time is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Childcare Network of Evanston",
+  "lat": 42.042466,
+  "lng": -87.698859,
+  "website": "http://www.childcarenetworkofevanston.org/",
+  "url": "https://youngchildrensite.files.wordpress.com/2016/11/child-care-providers.jpg?w=500&h=312&crop=1",
+  "text": "Childcare Network of Evanston is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Little Feet Childcare and Preschool",
+  "lat": 42.053689,
+  "lng": -87.691782,
+  "website": "http://www.littlefeetcc.com/joomla/",
+  "url": "https://static1.squarespace.com/static/57e7d4db44024351976cbf08/t/580d8558725e25bbaae1b37b/1477595445347/",
+  "text": "Little Feet Childcare and Preschool is a private preschool.",
+  "type": "privatepreschool"
+},
+{
+  "name": "Children’s Quest Home Daycare",
+  "lat": 42.040395,
+  "lng": -87.672026,
+  "website": "http://childcarecenter.us/provider_detail/childrens_quest_home_daycare_evanston_il",
+  "url": "http://achildgrows.com/wp-content/uploads/2015/11/toddler-brains.jpg",
+  "text": "Children’s Quest Home Daycare is a private preschool.",
+  "type": "privatepreschool"
+}
+];
+
+//private PRESCHOOL-8 SCHOOL points
+privateKEight = [
+{
+  "name": "Pope John Xxiii School (grades preschool-8)",
+  "lat": 42.032571,
+  "lng": -87.688406,
+  "website": "http://www.popejohn23.org/",
+  "url": "https://uploads.thealternativepress.com/uploads/photos/4a/best_3a8de1c78edb34054c66_Lynda_Wright_2.jpg",
+  "text": "Pope John Xxiii is a private school for preschool-8th grade students.",
+  "type": "privatepk-8"
+},
+{
+  "name": "St. Joan of Arc School (grades preschool-8)",
+  "lat": 42.049129,
+  "lng": -87.719809,
+  "website": "https://www.saintjoanofarc.net/school/",
+  "url": "https://images1.privateschoolreview.com/photo/780x600/9000/9378/St-Joan-Of-Arc-Elementary-School-bc7ee6d.jpg",
+  "text": "St. Joan of Arc is a private school for preschool-8th grade students.",
+  "type": "privatepk-8"
+},
+{
+  "name": "Chiaravalle Montessori School (grades preschool-8)",
+  "lat": 42.041452,
+  "lng": -87.678176,
+  "website": "https://www.chiaravalle.org/",
+  "url": "https://images2.privateschoolreview.com/photo/780x600/9000/8509/Chiaravalle-Montessori-School-j5H8XN.jpg",
+  "text": "Chiaravalle is a private Montessori school for preschool-8th grade students.",
+  "type": "privatepk-8"
+}
+];
+
+//private PRESCHOOL-12 points
+privatePKTwelve = [
+{
+  "name": "Roycemore School (grades preschool-12)",
+  "lat": 42.046547,
+  "lng": -87.689578,
+  "website": "http://www.roycemoreschool.org/",
+  "url": "http://www.roycemoreschool.org/sites/default/files/styles/panopoly_image_original/public/general/RWH_3785.JPG?itok=8MQYxWk9",
+  "text": "Roycemore is a private school for preschool-12th grade students.",
+  "type": "privatepk-12"
+}
+];
+
+//private K-8 SCHOOL points
+privateKEight = [
+{
+  "name": "St.Athanasius School (grades K-8)",
+  "lat": 42.063009,
+  "lng": -87.694339,
+  "website": "http://www.saintas.net/",
+  "url": "http://www.triblocal.com/evanston/files/2012/01/St-As-Kids.jpg",
+  "text": "St.Athanasius is a private school for kindergarten-8th grade students.",
+  "type": "privatek-8"
+}
+];
+
+//private 6-12 SCHOOL points
+privateSixTwelve = [
+{
+  "name": "Fusion Academy Evanston (grades 6-12)",
+  "lat": 42.033622,
+  "lng": -87.679184,
+  "website": "https://www.fusionacademy.com/fusion-campus-location/evanston/welcome/",
+  "url": "https://www.fusionacademy.com/wp-content/uploads/2015/08/FEG_WebsiteAbout2_1024x768-1024x768.jpg",
+  "text": "Fusion Academy is a private alternative school for 6th-12th grade students.",
+  "type": "private6-12"
+}
+];
+
+//private 9-12 SCHOOL points
+privateNineTwelve = [
+{
+  "name": "Beacon Academy (grades 9-12)",
+  "lat": 42.046409,
+  "lng": -87.680928,
+  "website": "http://beaconacademyil.org/",
+  "url": "http://beaconacademyil.org/wp-content/uploads/2015/05/lunchDiscussion.jpg",
+  "text": "Beacon Academy is a private alternative school for 9th-12th grade students.",
+  "type": "private9-12"
+}
+];
